8017239d32bb2135d490b45e6e325c65d47d831f
SadnanSadad <sadnan51-039@diu.edu.bd>
2024-10-31 17:14:37 +0600

    connected tp mongoDB

--- index.js
+++ index.js
@@ -13,9 +13,43 @@ app.use(
 );
 app.use(express.json());
 
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_User}:${process.env.DB_Pass}@cluster0.yuyym.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+
 app.get('/', (req, res) => {
-    res.send("Donate Life server is running");
-  })
-  app.listen(port, () => {
-    console.log(`Server running in port: ${port}`);
-  })
+  res.send("Donate Life server is running");
+})
+app.listen(port, () => {
+  console.log(`Server running in port: ${port}`);
+})
